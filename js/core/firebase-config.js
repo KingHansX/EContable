595df44859497d5d4c528a3f223a541a365e8383
KingHansX <92050593+KingHansX@users.noreply.github.com>
2026-01-19 13:16:26 -0500

    Agregar fallback para métodos find() y findById() en inicialización

--- js/core/firebase-config.js
+++ js/core/firebase-config.js
@@ -8,8 +8,11 @@ window.firebaseConfig = {
     apiKey: "AIzaSyDBpSNaXo_z6Z22PNmAPHkBaSaTQM-jrCo",
     authDomain: "econtable-96a54.firebaseapp.com",
     projectId: "econtable-96a54",
+    databaseURL: "https://econtable-96a54.firebaseio.com",
     storageBucket: "econtable-96a54.firebasestorage.app",
     messagingSenderId: "175885561564",
     appId: "1:175885561564:web:ea5bf0682030fa1a5f4e19",
     measurementId: "G-RVP5MDBSCM"
 };
+
+console.log('🔧 Firebase config cargado');
